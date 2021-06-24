ef7da53cfcecd3e5d340a092ab20f5244a918c46
pixel-yo <83309151+pixel-yo@users.noreply.github.com>
2021-06-24 16:21:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js'),
     config = require('./config.json'),
     fs = require('fs')
  
-client.login(env.process.TOKEN)
+client.login(process.env.TOKEN)
 client.commands = new Discord.Collection()
 client.db = require('./db.json')
 
